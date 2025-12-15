d536f0a33ef66acc3101d53df9a38af00d0da2d8
yassmine elkhder <yassmineelkhder@gmail.com>
2025-12-15 15:49:27 +0100

    chore: add GitHub Actions workflow

--- chat/tests.py
+++ chat/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
 
 # Create your tests here.
+from .models import Message
+
+class ChatModelTest(TestCase):
+    def test_message_creation(self):
+        msg = Message.objects.create(content="Hello")
+        self.assertEqual(msg.content, "Hello")
